cfaebda6cbab2691bd42eb5b0e1f23734349a787
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-06-16 23:43:33 +0530

    Adds edittext and search button to web view activity

--- app/src/main/java/android/com/buyhatke/MainActivity.java
+++ app/src/main/java/android/com/buyhatke/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private CustomTabsClient mClient;
@@ -33,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     runChromeCustomTab();
                 } catch (Exception e) {
-                    Toast.makeText(getBaseContext(), "error", Toast.LENGTH_SHORT).show();
                     runViaWebView();
                 }
             }
--- app/src/main/java/android/com/buyhatke/WebViewActivity.java
+++ app/src/main/java/android/com/buyhatke/WebViewActivity.java
@@ -1,12 +1,14 @@
 package android.com.buyhatke;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * Created by shobhit on 16/6/17.
@@ -16,6 +18,8 @@ public class WebViewActivity extends AppCompatActivity {
 
     private static final String TAG = "WebViewActivity";
     private WebView webView;
+    private ImageButton button;
+    private EditText editText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,14 +29,24 @@ public class WebViewActivity extends AppCompatActivity {
         String url = getIntent().getExtras().getString("URL");
 
         webView = (WebView) findViewById(R.id.webView);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (ImageButton) findViewById(R.id.button);
+
+        editText.setText(url);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startWebView(editText.getText().toString());
+            }
+        });
+
         startWebView(url);
     }
 
     private void startWebView(final String url) {
 
         webView.setWebViewClient(new WebViewClient() {
-
-            ProgressDialog progressDialog;
 
             //If you will not use this method url links are open in new browser not in webview
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -42,22 +56,11 @@ public class WebViewActivity extends AppCompatActivity {
 
             public void onLoadResource(WebView view, String url) {
                 Log.d(TAG, "onLoadResource: " + url);
-                if (progressDialog == null) {
-                    progressDialog = new ProgressDialog(WebViewActivity.this);
-                    progressDialog.setMessage("Loading...");
-                    //progressDialog.show();
-                }
             }
 
             public void onPageFinished(WebView view, String url) {
-                try {
-                    if (progressDialog.isShowing()) {
-                        progressDialog.dismiss();
-                        progressDialog = null;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                Log.d(TAG, "onPageFinished: " + url);
+                editText.setText(url);
             }
 
         });
